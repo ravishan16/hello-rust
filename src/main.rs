acc0eb8ca304b424ce713a7f375752f1db1562d1
Ravishankar Sivasubramaniam <ravi_siva@live.com>
2019-10-17 14:29:51 -0400

    Adding dependency ferries_says

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+extern crate ferris_says;
+
+use ferris_says::say;
+use std::io::{stdout, BufWriter};
+
 fn main() {
-    println!("Hello, world!");
+    let out = b"Hello fellow Rustaceans!";
+    let width = 24;
+
+    let mut writer = BufWriter::new(stdout());
+    say(out, width, &mut writer).unwrap();
 }
